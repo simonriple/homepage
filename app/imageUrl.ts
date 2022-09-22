f38b4c459ec8becf9dc2d2b7f0a83c10c56e27f4
Simon Riple <simon.riple@knowit.no>
2022-09-22 21:01:51 +0200

    fix build errors

--- app/imageUrl.ts
+++ app/imageUrl.ts
@@ -8,6 +8,6 @@ const builder = imageUrlBuilder(client)
 // Then we like to make a simple function like this that gives the
 // builder an image and returns the builder for you to specify additional
 // parameters:
-export function urlFor(source) {
+export function urlFor(source: any) {
   return builder.image(source)
 }
