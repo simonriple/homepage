28f07fd9550642ce2568c3567bdc051d519a30d6
simonriple <simon.riple@gmail.com>
2024-03-21 11:52:01 +0100

    build errors

--- app/modules/illustration/CanvasIllustration.tsx
+++ app/modules/illustration/CanvasIllustration.tsx
@@ -1,16 +1,16 @@
-// import { Box } from '@chakra-ui/react'
-// import { Canvas } from '@react-three/fiber'
-// import { Suspense } from 'react'
-// import { Plane } from './Plane'
-// export const CanvasIllustration = () => {
-//   return (
-//     <Box position='absolute' top={0} height='80dvh' width='100%' zIndex={-10}>
-//       <Canvas camera={{ position: [0, 2.5, 6] }}>
-//         <ambientLight />
-//         <Suspense fallback={null}>
-//           <Plane state={'bergen'} />
-//         </Suspense>
-//       </Canvas>
-//     </Box>
-//   )
-// }
+import { Box } from '@chakra-ui/react'
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react'
+import { Plane } from './Plane'
+export const CanvasIllustration = () => {
+  return (
+    <Box position='absolute' top={0} height='80dvh' width='100%' zIndex={-10}>
+      <Canvas camera={{ position: [0, 2.5, 6] }}>
+        <ambientLight />
+        <Suspense fallback={null}>
+          <Plane state={'bergen'} />
+        </Suspense>
+      </Canvas>
+    </Box>
+  )
+}
